489cb51c5dedf05ba6a2f57137d9ad18a77b26f6
Kyle <k7for9.eckrich@gmail.com>
2022-12-01 01:15:44 -0600

    day 1 q 2 done

--- day1/problem1.py
+++ day1/problem1.py
@@ -5,15 +5,18 @@ f = open("day1\input.txt", "r")
 
 # add the numbers together until line is empty then update max
 
-max = 0
 total = 0
+top_three = [0, 0, 0]
 
 for line in f:
     if line == "\n":
-        if total > max:
-            max = total
+        current_min = min(top_three)
+        if total > current_min:
+            top_three.remove(current_min)
+            top_three.append(total)
         total = 0
         continue
     total += int(line)
 
-print(max)
+print("max: ", max(top_three))
+print("sum of top three: ", sum(top_three))
