822c252b0e37eaf02e98f7e9d3e64c0ccfc1d4f5
Kyle <k7for9.eckrich@gmail.com>
2022-12-01 23:21:30 -0600

    d2 p2 done

--- day2/problem2.py
+++ day2/problem2.py
@@ -24,12 +24,31 @@ print("sum of top three: ", sum(top_three))
 '''
 
 
+def choosemove(p1, p2):
+    if p2 == "Y":
+        return chr(ord(p1)+23)
+    if p1 == "A":
+        if p2 == "X":
+            return "Z"
+        else:
+            return "Y"
+    if p1 == "B":
+        if p2 == "X":
+            return "X"
+        else:
+            return "Z"
+    if p1 == "C":
+        if p2 == "X":
+            return "Y"
+        else:
+            return "X"
 
 
 sum = 0 
 for line in f:
     p1 = line[0]
     p2 = line[2]
+    p2 = choosemove(p1, p2)
     if ord(p1) == ord(p2)-23:
         sum += ord(p1) + 3 - 64
     elif p1 == "A" and p2 == "Y":
